9c5bb426964a25b35dd806aa6043d12f3de4090d
Jesus Alcocer Tagua <jesalctag@gmail.com>
2023-12-29 14:11:20 +0100

    js

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,13 +1,13 @@
-// // src/utils/baseUrl.js
-// export function baseUrl(path) {
-//     const basePath = '/web3'; // Set your base path here
-//     return `${basePath}${path}`;
-//   }
+// src/utils/baseUrl.js
+export function baseUrl(path) {
+    const basePath = '/web3'; // Set your base path here
+    return `${basePath}${path}`;
+  }
 
 // src/utils/baseUrl.js
-import { getConfig } from 'astro/config';
+// import { getConfig } from 'astro/config';
 
-export function baseUrl(path) {
-  const { base } = getConfig();
-  return `${base}${path}`;
-}
+// export function baseUrl(path) {
+//   const { base } = getConfig();
+//   return `${base}${path}`;
+// }
